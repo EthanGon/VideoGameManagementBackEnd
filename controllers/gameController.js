11dd38b7278fae085c6d6434f55c087478961f03
Ethan Gonzalez <ethangonzalez003@gmail.com>
2025-05-15 15:04:18 -0400

    able to get a game based on it's igdb id and if the id does not exist an error response is sent instead

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -7,7 +7,47 @@ module.exports = {
     res.status(200).json(games);
   },
   getGame: async (req, res) => {
-    res.status(200).json("getGame method");
+    try {
+      const { id } = req.params;
+
+      const query = `fields name, id, summary, cover.image_id; where id = ${id};`;
+
+      const response = await fetch(BASEURL, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Client-ID": process.env.VITE_TWITCH_CLIENT_ID,
+          Authorization: `Bearer ${process.env.VITE_TWITCH_ACCESS_TOKEN}`,
+        },
+        body: query,
+      });
+
+      const json = await response.json();
+
+      const games = json.map((game) => {
+        const imageId = game.cover?.image_id;
+        return {
+          gameId: game.id,
+          title: game.name,
+          description: game.summary,
+          cover: imageId
+            ? `https://images.igdb.com/igdb/image/upload/t_cover_big_2x/${imageId}.jpg`
+            : null,
+        };
+      });
+
+      if (!response.ok) {
+        throw Error(`The game with id = ${id} could not be found.`);
+      }
+
+      const game = await Game.findOne({ gameId: id });
+
+      const status = game ? game.status : null;
+
+      res.status(200).json({ ...games[0], status });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   },
   searchGames: async (req, res) => {
     try {
