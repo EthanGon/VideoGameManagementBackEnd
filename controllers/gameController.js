1c76c8be1b81c55f1bad48c649b37bdfdc0b0955
Ethan Gonzalez <ethangonzalez003@gmail.com>
2025-05-15 14:33:24 -0400

    able to search for games from the igdb api

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,3 +1,5 @@
+const BASEURL = "https://api.igdb.com/v4/games";
+
 module.exports = {
   getGames: async (req, res) => {
     res.status(200).json("getGames method");
@@ -6,7 +8,37 @@ module.exports = {
     res.status(200).json("getGame method");
   },
   searchGames: async (req, res) => {
-    res.status(200).json("searchGames method");
+    try {
+      const query = `search "${req.params.query}"; fields name, id, summary, cover.image_id;`;
+
+      const response = await fetch(BASEURL, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Client-ID": process.env.VITE_TWITCH_CLIENT_ID,
+          Authorization: `Bearer ${process.env.VITE_TWITCH_ACCESS_TOKEN}`,
+        },
+        body: query,
+      });
+
+      const json = await response.json();
+
+      const games = json.map((game) => {
+        const imageId = game.cover?.image_id;
+        return {
+          gameId: game.id,
+          title: game.name,
+          description: game.summary,
+          cover: imageId
+            ? `https://images.igdb.com/igdb/image/upload/t_cover_big_2x/${imageId}.jpg`
+            : null,
+        };
+      });
+
+      res.status(200).json(games);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   },
   addGame: async (req, res) => {
     res.status(200).json("addGame method");
