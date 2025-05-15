c8bf79c5d7d479935bbe47f509ceb7b825e9ae03
Ethan Gonzalez <ethangonzalez003@gmail.com>
2025-05-15 13:08:19 -0400

    installed all dependencies and able to start a server that listens on a port

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+// Load config
+require("dotenv").config({ path: "./config/.env" });
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
