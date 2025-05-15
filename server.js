4bf2bbfd172cb4a55fd9cc66a65972ae010adbe4
Ethan Gonzalez <ethangonzalez003@gmail.com>
2025-05-15 13:18:33 -0400

    added game routes to the server

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const gameRoutes = require("./routes/game");
 
 // Load config
 require("dotenv").config({ path: "./config/.env" });
+
+// Routes
+app.use("/api/games", gameRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
